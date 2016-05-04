06f436a0c3ccfc44d335689a18efc7624f3357b3
Alexander Christiansson <alexander.christiansson@gmail.com>
2016-05-04 11:00:30 +0200

    Import describe and it from global scope instead of mocha
    Watch mode breaks otherwise. See
    https://github.com/mochajs/mocha/issues/956.

--- test/insertCssSpec.js
+++ test/insertCssSpec.js
@@ -8,9 +8,10 @@
  */
 
 import jsdom from 'jsdom';
-import { describe, it } from 'mocha';
 import { expect } from 'chai';
 import insertCss from '../src/insertCss';
+
+const { describe, it } = global;
 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = document.parentWindow;
