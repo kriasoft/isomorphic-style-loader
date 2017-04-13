d59481b6295783f9449cf720f86ed698a5b1fb4e
Paul LeMarquand <plemarquand@qnx.com>
2017-04-12 20:45:54 -0400

    Fixes for React 15.5.0
    - Pull PropTypes from prop-types package
    - Use createClass from create-react-class
    
    Fixes #89

--- src/withStyles.js
+++ src/withStyles.js
@@ -7,7 +7,8 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import hoistStatics from 'hoist-non-react-statics';
 
 const contextTypes = {
