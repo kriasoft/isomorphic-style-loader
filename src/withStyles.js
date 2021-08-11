3283503883db21bf03dfbfcae2f4c745816cf74c
Silvio <slopes321@yahoo.com>
2021-08-11 04:11:59 -0700

    Check that removeCss is a function before calling it (#138)
    Co-authored-by: Michael Duve <mduve@designmail.net>

--- src/withStyles.js
+++ src/withStyles.js
@@ -22,7 +22,7 @@ function withStyles(...styles) {
       }
 
       componentWillUnmount() {
-        if (this.removeCss) {
+        if (typeof this.removeCss === 'function') {
           setTimeout(this.removeCss, 0)
         }
       }
